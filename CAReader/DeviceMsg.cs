123c0626b47dc2490fb1cfdc2316611c1010e751
Erica Barone <erica.barone@hotmail.com>
2017-06-29 10:41:38 +0200

    adding Reader

---
+++ CAReader/DeviceMsg.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CAReader
+{
+    class DeviceMsg
+    {
+        public string DeviceID { get; set; }
+        public int MsgID { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public object Data { get; set; }
+        //public double Temperature { get; set; }
+        //public double Humidity { get; set; }
+        //public bool DoorOpen { get; set; }
+
+
+    }   
+}
---
+++ CAReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.ServiceBus.Messaging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CAReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Reader - reads messages from Q1. Ctrl-C to exit.\n");
+            var connectionString = "Endpoint=sb://ebsbnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=59Oq2KM+b5DNqRsoQ+qbua5Z7zG/7I/ohAHukC9eaKA=";
+            var queueName = "sbqueue1";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                Stream stream = message.GetBody<Stream>();
+                StreamReader reader = new StreamReader(stream, Encoding.ASCII);
+                string s = reader.ReadToEnd();
+
+                var msg = JsonConvert.DeserializeObject(s);
+                JObject json = JObject.Parse(s);
+
+                DeviceMsg deviceMsg = new DeviceMsg();
+
+                deviceMsg.DeviceID = json["deviceId"].Value<string>();
+                deviceMsg.MsgID = json["messageId"].Value<int>();
+                deviceMsg.TimeStamp = message.EnqueuedTimeUtc;
+                deviceMsg.Data = msg;
+            
+                Console.WriteLine(String.Format("Message body: {0}", s));
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
